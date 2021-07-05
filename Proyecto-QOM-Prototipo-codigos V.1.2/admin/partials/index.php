38bfe58000af4a196925b63ba48e9fc2ff31ee5d
Jennifer Cetina <74010102+JenniLCetinaS@users.noreply.github.com>
2021-07-04 20:52:15 -0500

    Add files via upload

---
+++ Proyecto-QOM-Prototipo-codigos V.1.2/admin/partials/index.php
@@ -0,0 +1,70 @@
+
+<html>
+    <head><title>QOM-Pagina de inicio</title>
+        <link rel="stylesheet" href="../css/admin.css"/>
+    </head>
+    <body>
+        <!--Seccion Menu empieza aqui-->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Inicio</a><li>
+                    <li><a href="#">Admin</a><li>
+                    <li><a href="#">Categoria</a><li>
+                    <li><a href="#">Productos</a><li>
+                    <li><a href="#">Pedido</a><li>
+                </ul>
+            </div>
+            
+        </div>
+        <!--Seccion Menu termina aqui-->
+
+         <!--Seccion contenido principal aqui-->
+         <div class="contenido-principal">
+            <div class="wrapper">
+                <h1>Barra de navegación</h1>
+
+                <div class="color text-center">
+                    <h1>1</h1>
+                    <br />
+                    categorias
+                </div>
+
+                <div class="color text-center">
+                    <h1>2</h1>
+                    <br />
+                    categorias
+
+                </div>
+                
+                <div class="color text-center">
+                    <h1>3</h1>
+                    <br />
+                    categorias
+
+                </div>
+                
+                <div class="color text-center">
+                    <h1>4</h1>
+                    <br />
+                    categorias
+
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+        
+        <!--Seccion contenido principtal termina aqui-->
+
+        <!--Seccion pie de pagina empieza aqui-->
+      <div class="pie_depagina">
+         <div class="wrapper text-center">
+                <p>Diseñado por <a href="#">Juan Valdes</a></p>
+            </div>
+        </div>
+        
+        <!--Seccion pie de pagina termina aqui-->
+    </body>
+</html>
